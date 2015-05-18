70a694085412a7d3faca7424867fff076a19f078
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-05-18 23:52:39 +0900

    Added random matrix example

---
+++ linear_system/random_matrix.c
@@ -0,0 +1,34 @@
+#include "matrix_util.h"
+#include "mersenne_twister.h"
+#include <stdio.h>
+
+int main() {
+  int m, n;
+  int i, j;
+  double **mat;
+  double *v;
+
+  m = 1000;
+  n = 1000;
+
+  /* 乱数(Mersenne Twister)の初期化 */
+  init_genrand(12345);
+
+  /* 要素が [-1,1] のランダム行列の生成 */
+  mat = alloc_dmatrix(m, n);
+  for (i = 0; i < m; ++i) {
+    for (j = 0; j < n; ++j) {
+      mat[i][j] = 2.0 * genrand_real1() - 1.0;
+    }
+  }
+  fprint_dmatrix(stdout, m, n, mat);
+  free_dmatrix(mat);
+
+  /* 要素が [-1,1] のランダムベクトルの生成 */
+  v = alloc_dvector(n);
+  for (i = 0; i < n; ++i) {
+    v[i] = 2.0 * genrand_real1() - 1.0;
+  }
+  fprint_dvector(stdout, n, v);
+  free_dvector(v);
+}
