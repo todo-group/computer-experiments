bf50364417a621ee51f907625814a48ff1208c0f
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2015-05-19 09:19:56 +0900

    Merged pointer_vector and pointer_matrix

--- linear_system/pointer.c
+++ linear_system/pointer.c
@@ -3,12 +3,38 @@
 
 int main() {
   int n, i, j;
+  double *v;
   double **m;
   n = 10;
+
+  /* test for vector */
+  v = alloc_dvector(n);
+  for (i = 0; i < n; ++i) v[i] = i;
+  fprint_dvector(stdout, n, v);
+  
+  printf("v     = %lu\n", (long)v);
+  printf("&v[0] = %lu\n", (long)&v[0]);
+
+  printf("(v+2) = %lu\n", (long)(v+2));
+  printf("&v[2] = %lu\n", (long)&v[2]);
+
+  printf("*v       = %10.5f\n", *v);
+  printf("v[0]     = %10.5f\n", v[0]);
+
+  printf("*(v+2)   = %10.5f\n", *(v+2));
+  printf("v[2]     = %10.5f\n", v[2]);
+
+  printf("(v+2)[3] = %10.5f\n", (v+2)[3]);
+  printf("*(v+2+3) = %10.5f\n", *(v+2+3));
+
+  free_dvector(v);
+  
+  /* test for matrix */
   m = alloc_dmatrix(n, n);
   for (i = 0; i < n; ++i)
     for (j = 0; j < n; ++j)
       m[i][j] = 100 * i + j;
+  fprint_dmatrix(stdout, n, n, m);
   
   printf("m        = %lu\n", (long)m);
   printf("&m[0]    = %lu\n", (long)&m[0]);
