d35fb799303052ca26a840a9fa48b146ed9e0e84
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2018-04-16 10:17:51 +0900

    Preparing lecture-1-1

--- lecture/makedepend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-INPUTS=$(grep '\\input' *.tex | sed 's/[{:}]/ /g' | sed 's/ \\input /:/')
-
-for i in $INPUTS; do
-    TARGET=$(echo $i | awk -F: '{print $1}' | sed 's/.tex/.pdf/')
-    SOURCE=$(echo $i | awk -F: '{print $2}')
-    echo "$TARGET: $SOURCE"
-done
