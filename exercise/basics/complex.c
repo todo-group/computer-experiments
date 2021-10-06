bf189b7535662ced6d5eb3ccbbefb79c5b995b69
Synge Todo <wistaria@phys.s.u-tokyo.ac.jp>
2021-10-06 09:35:04 +0900

    remove handbook

--- exercise/basics/complex.c
+++ exercise/basics/complex.c
@@ -4,7 +4,7 @@
 
 int main() {
   double complex x, y;
-  x = CMPLX(0, 1); /* 虚数単位 */
+  x = 0 + 1 * I; /* 虚数単位 */
   y = cexp(x * M_PI);
   printf("i = (%lf,%lf)\n", creal(x), cimag(x));
   printf("e^{i*pi} = (%lf,%lf)\n", creal(y), cimag(y));
